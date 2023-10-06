bb1129b30852077172138f3534aa45a5de08abaa
[Urwah shahab] <[f200389@cfd.nu.edu.pk]>
2023-10-06 09:59:43 +0500

    Calculator that performs addition and subtraction on x and y

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int x = 21;  
+        int y = 15;   
+
+        
+        int addition = x + y;
+        int subtraction = x - y;
+     
+
+        
+        System.out.println("Addition of given numbers is: " + addition);
+        System.out.println("Subtraction of given numbers is: " + subtraction);
+      
+    }
+}
